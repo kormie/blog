9c19acab6d9be5716f6cc3dfc356c9d85a2fe158
David <agile_check_in@Davids-MacBook-Pro-2.local>
2012-09-30 11:31:06 -0500

    Add nasty JS helper.... woof

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def render_disqus_javascript
+    js = "<div id='disqus_thread'></div>"
+    js += javascript_tag do
+      disqus_shortname = 'komie'
+      "(function() { var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true; dsq.src = 'http://' + '#{disqus_shortname}' + '.disqus.com/embed.js'; (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq); })();"
+    end
+    js += "<noscript>Please enable JavaScript to view the <a href='http://disqus.com/?ref_noscript'>comments powered by Disqus.</a></noscript>"
+    js += "<a href='http://disqus.com' class='dsq-brlink'>comments powered by <span class='logo-disqus'>Disqus</span></a>"
+  end
 end
